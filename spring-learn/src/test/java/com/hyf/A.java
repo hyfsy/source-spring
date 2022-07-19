7aa61b9a52629337b34d62a51f7f1705ace07fda
hyfsy <1577975140@qq.com>
2022-07-19 18:44:00 +0800

    最后一次添加环境依赖

---
+++ spring-learn/src/test/java/com/hyf/A.java
@@ -0,0 +1,17 @@
+package com.hyf;
+
+import org.junit.Test;
+import org.springframework.context.annotation.DependsOn;
+
+/**
+ * @author baB_hyf
+ * @date 2021/11/05
+ */
+@DependsOn({"a", "a"})
+public class A {
+
+	@Test
+	public void testA() {
+
+	}
+}
